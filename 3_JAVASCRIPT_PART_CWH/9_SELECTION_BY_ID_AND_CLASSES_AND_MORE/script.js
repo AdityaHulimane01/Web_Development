a506667626910d9a1ca3e4505a70dfd3cbee85a6
Aditya Hulimane <adityasanjayhulimane@gmail.com>
2026-09-20 18:49:02 +0530

    Add JS

---
+++ 3_JAVASCRIPT_PART_CWH/9_SELECTION_BY_ID_AND_CLASSES_AND_MORE/script.js
@@ -0,0 +1,12 @@
+// let boxes = document.getElementsByClassName("box");
+// console.log(boxes);
+
+// boxes[2].style.backgroundColor = "red"
+
+// document.getElementById("greenBox").style.backgroundColor = "green"
+
+// document.querySelector(".box").style.backgroundColor = "blue"
+
+document.querySelectorAll(".box").forEach(e =>{
+    e.style.backgroundColor = "yellow"
+})
